30593474a4e5511321c845748eaec48273640f2c
Luis Rivera <lchoezr3@gmail.com>
2024-10-10 14:44:09 +0200

    Subida inicial de archivos
    Todos los archivos subidos

---
+++ prueba.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class prueba here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class prueba
+{
+    public static void main(String)[] args)
+    }
+}
